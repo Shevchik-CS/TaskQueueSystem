ca2d7c5e089e31d6d044a083797f6fc79be288b6
Shevchik-CS <serg2443537@gmail.com>
2024-03-25 21:57:00 +0200

    fix testing workflow

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# pylint: disable=missing-module-docstring
+from task_queue_system import Task, Resources, TaskQueue, TaskQueueException
+
+__all__ = ["Task", "Resources", "TaskQueue", "TaskQueueException"]
